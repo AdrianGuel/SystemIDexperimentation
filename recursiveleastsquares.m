75e87312135371aed9adc05a9f625cc8315b6771
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2020-12-14 13:21:55 +0000

    Update recursiveleastsquares.m

--- recursiveleastsquares.m
+++ recursiveleastsquares.m
@@ -1,4 +1,7 @@
 %Recursive least squares
+%Adrian-Josue Guel-Cortez 2020
+%If you use my codes do not forget to cite me
+%https://scholar.google.com.mx/citations?user=gZcBLuoAAAAJ&hl=es
 function [theta_k,P_k,f_k]=recursiveleastsquares(u_k,y_k,P,theta,f_k,a,gamma,n)
     L_k=(1/gamma)*P*f_k./((1/a)+(1/gamma)*f_k'*P*f_k);
     theta_k=theta+L_k*(y_k-f_k'*theta);
